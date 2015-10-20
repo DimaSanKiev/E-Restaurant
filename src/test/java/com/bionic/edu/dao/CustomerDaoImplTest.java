71a5e13e0335ac65df68068d985a4bed5e473a23
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-10-20 23:43:00 +0300

    add dao tests

--- src/test/java/com/bionic/edu/dao/CustomerDaoImplTest.java
+++ src/test/java/com/bionic/edu/dao/CustomerDaoImplTest.java
@@ -15,16 +15,17 @@ import static org.junit.Assert.assertNotNull;
 public class CustomerDaoImplTest {
 
     CustomerDao customerDao;
+    ApplicationContext context;
 
     @Before
     public void setUp() throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/application-context.xml");
+        context = new ClassPathXmlApplicationContext("META-INF/application-context.xml");
         customerDao = (CustomerDao) context.getBean("customerDaoImpl");
     }
 
     @After
     public void tearDown() throws Exception {
-
+    
     }
 
     @Test
@@ -50,17 +51,20 @@ public class CustomerDaoImplTest {
 
     @Test
     public void testAdd() throws Exception {
-
+    	Customer customer = new Customer();
+    	customerDao.add(customer);
+    	assertNotNull(customerDao.findById(6));
     }
 
     @Test
     public void testUpdate() throws Exception {
-
+    	
     }
 
     @Test
     public void testDelete() throws Exception {
-
+    	customerDao.delete(6);
+    	assertEquals(5, customerDao.findAll().size());
     }
 
     @Test

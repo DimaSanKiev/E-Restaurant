d51d4bbddfc24aeb462c21bdff2213cdca202831
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-06-01 22:22:47 +0300

    moved to annotation based configuration

---
+++ src/main/java/com/bionic/edu/configuration/AppConfig.java
@@ -0,0 +1,38 @@
+package com.bionic.edu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+@ImportResource("classpath:spring/hibernate-context.xml")
+@ComponentScan(basePackages = {"com.bionic.edu"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver jsfViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/");
+        bean.setSuffix(".xhtml");
+        return bean;
+    }
+}
